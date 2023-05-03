79a5f4ad546c8af0f00ab96002cc575642d0e95d
gsanchez85 <gsanchezcalvodev@gmail.com>
2023-05-03 19:32:10 +0200

    upload project

---
+++ src/components/Ficha.jsx
@@ -0,0 +1,20 @@
+const ficha = [
+    { 
+      'name': 'Hook',
+      'owner': 'Germán',
+      'email': 'correo@correo.com',
+      'date': '2022-12-10',
+      'symptoms': 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, ' +
+                  'totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. ' +
+                  'Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos.'
+    },  
+    { 
+      'name': 'Gea',
+      'owner': 'Ana',
+      'email': 'correo@example.com',
+      'date': '2022-11-30',
+      'symptoms': 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.'
+    }  
+  ];
+
+  export default ficha;
